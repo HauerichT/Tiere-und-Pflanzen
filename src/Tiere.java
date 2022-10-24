c26891973d1d11468e1a9509739b51bfa17b03a7
timohaverich <timo.haverich@fh-bielefeld.de>
2022-10-24 21:43:06 +0200

    int

--- src/Tiere.java
+++ src/Tiere.java
@@ -1,7 +1,29 @@
 public class Tiere {
     private String bezeichnung;
-    private String nahrung;
-    private String nahrungTiere;
-    private String nahrungPflanzen;
+    private String nahrungTiere = "-";
+    private String nahrungPflanzen = "-";
 
+    public static void addNahrungTiere(String tier) {
+        nahrungTier = tier;
+    }
+
+    public static void addNahrungPflanzen(String pflanze) {
+        nahrungPflanzen = pflanze;
+    }
+
+    public static void checkNahrungsart(String pNahrungTiere, String pNahrungPflanzen) {
+        if (pNahrungTiere.equals("-") && !pNahrungPflanzen.equals("-")) {
+            System.out.println(bezeichnung + "ist ein Pflanzenfresser");
+        }
+        else if (!pNahrungTiere.equals("-") && pNahrungPflanzen.equals("-")) {
+            System.out.println(bezeichnung + "ist ein Fleischfresser");
+        }
+        else if (!pNahrungTiere.equals("-") && !pNahrungPflanzen.equals("-")) {
+            System.out.println(bezeichnung + "ist ein Allesfresser");
+        }
+    }
+
+    public static void setBezeichnung(String pBezeichnung) {
+        bezeichnung = pBezeichnung;
+    }
 }
